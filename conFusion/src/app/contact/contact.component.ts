3633fae4e9336f1ffcf5d4b15f7a8be61d0a3836
Ataullah Masum <masumruet1@gmail.com>
2020-11-11 07:26:29 +0600

    complete website

--- conFusion/src/app/contact/contact.component.ts
+++ conFusion/src/app/contact/contact.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-import { Feedback, ContactType } from '../shared/feedback';
-import { flyInOut  } from '../animations/app.animation';
+import { Component, OnInit , ViewChild} from '@angular/core';
+import {FormBuilder, FormGroup , Validators} from '@angular/forms';
+import {Feedback, ContactType} from '../shared/feedback';
+import {expand, flyInOut} from '../animations/app.animation';
+import {FeedbackService} from '../services/feedback.service';
 
 
 @Component({
@@ -12,84 +12,85 @@ import { flyInOut  } from '../animations/app.animation';
   host: {
     '[@flyInOut]': 'true',
     'style': 'display: block;'
-    },
-    animations: [
-      flyInOut()
-    ]
+  },
+  animations: [
+    flyInOut()
+    , expand()
+  ]
 })
 export class ContactComponent implements OnInit {
-  
   @ViewChild('fform') feedbackFormDirective;
-
   feedbackForm: FormGroup;
   feedback: Feedback;
+  feedbackCopy: Feedback;
   contactType = ContactType;
+  errMess: string;
+  submited: Boolean;
+  waiting: Boolean;
   formErrors = {
     'firstname': '',
     'lastname': '',
     'telnum': '',
-    'email': ''
+     'email': ''
   };
-
   validationMessages = {
     'firstname': {
-      'required':      'First Name is required.',
-      'minlength':     'First Name must be at least 2 characters long.',
-      'maxlength':     'FirstName cannot be more than 25 characters long.'
+      'required': 'First name is required',
+      'minlength': 'First name must be at least two characters long',
+      'maxlength': 'First name cannot be more than 25 characters'
     },
     'lastname': {
-      'required':      'Last Name is required.',
-      'minlength':     'Last Name must be at least 2 characters long.',
-      'maxlength':     'Last Name cannot be more than 25 characters long.'
+      'required': 'Last name is required',
+      'minlength': 'Last name must be at least two characters long',
+      'maxlength': 'Last name cannot be more than 25 characters'
     },
     'telnum': {
-      'required':      'Tel. number is required.',
-      'pattern':       'Tel. number must contain only numbers.'
+      'required' : 'Tel. number is required',
+      'pattern' : 'Tel. number must contain only numbers'
     },
     'email': {
-      'required':      'Email is required.',
-      'email':         'Email not in valid format.'
-    },
+      'required' : 'email is required',
+      'email' : 'email not in valid format.'
+    }
   };
-
-  constructor(private fb: FormBuilder) { 
+  constructor(private fb: FormBuilder,
+  private feedbackService: FeedbackService) {
     this.createForm();
   }
 
   ngOnInit() {
   }
+
   createForm() {
     this.feedbackForm = this.fb.group({
       firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
       lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
-      telnum: ['', [Validators.required, Validators.pattern] ],
-      email: ['', [Validators.required, Validators.email] ],
+      telnum: ['', Validators.required, Validators.pattern ],
+      email: ['', Validators.required , Validators.email],
       agree: false,
       contacttype: 'None',
       message: ''
     });
     this.feedbackForm.valueChanges
-    .subscribe(data => this.onValueChanged(data));
-
-    this.onValueChanged(); // (re)set validation messages now
-
+      .subscribe(data => this.onValueChanged(data));
+  this.onValueChanged();
   }
   onValueChanged(data?: any) {
     if (!this.feedbackForm) { return; }
     const form = this.feedbackForm;
     for (const field in this.formErrors) {
       if (this.formErrors.hasOwnProperty(field)) {
-        // clear previous error message (if any)
         this.formErrors[field] = '';
         const control = form.get(field);
         if (control && control.dirty && !control.valid) {
           const messages = this.validationMessages[field];
           for (const key in control.errors) {
             if (control.errors.hasOwnProperty(key)) {
-              this.formErrors[field] += messages[key] + ' ';
+              this.formErrors[field] += messages[key] + ' ' ;
             }
           }
         }
+
       }
     }
   }
@@ -97,6 +98,19 @@ export class ContactComponent implements OnInit {
   onSubmit() {
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
+    this.submited = true;
+    this.waiting = true;
+    this.feedbackCopy = this.feedbackForm.value;
+    this.feedbackService.submitFeedback(this.feedbackCopy)
+      .subscribe(feed => {
+        this.feedback = feed;
+        this.feedbackCopy = feed;
+        this.waiting = false;
+          setTimeout(() => {
+            this.submited = false;
+          }, 5000);
+      },
+        errmess => { this.feedback = null; this.feedbackCopy = null; this.errMess = <any>errmess; });
     this.feedbackForm.reset({
       firstname: '',
       lastname: '',
@@ -108,4 +122,6 @@ export class ContactComponent implements OnInit {
     });
     this.feedbackFormDirective.resetForm();
   }
+
+
 }
